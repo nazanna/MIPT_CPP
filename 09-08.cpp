0636e13b89df89de763d7d3727ac01677f80e0f8
anna <anna8g5@mail.ru>
2022-09-18 15:16:00 +0300

    new files

--- 09-08.cpp
+++ 09-08.cpp
@@ -4,6 +4,7 @@
 using std::cout;
 using std::cin;
 using std::endl;
+using std::string;
 
 //быстрая сортировка
 /*generic_qsort(void* begin, size_t element_byte_size, bool (*cmp)(*void const*, void const*, void const*), void* end, void const*){
@@ -22,109 +23,165 @@ void poll(PriorityQueue* q){/*убирает макс}
 void const* peek(PriorityQueue* q){/*возвращает макс}
 bool is_empty(PriorityQueue* q){}*/
 
-bool is_lower(int* a, int* b, void* env = nullptr) {
-    return *a < *b;
+bool is_lower(void* a, void* b, void* env = nullptr) {
+    auto ia = *(int*)(a);
+    auto ib = *(int*)(b);
+    return ia < ib;
 }
+
+void swap(void *a, void *b, size_t element_byte_size) {
+    void* temp = malloc(element_byte_size);
+    memcpy(temp, a, element_byte_size);
+    memcpy(a, b, element_byte_size);
+    memcpy(b, temp, element_byte_size);
+    free(temp);
+}
+
 
 struct Heap{
-    int* start;
+    void* memory;
     void* env;
-    Heap() {
-        // cout << "im default constructor\n";
-        start = new int[1000];
-        for (int i = 0; i < 1000; ++i) start[i] = 0;
-    }
+    Heap(bool (*cmp)(void*, void*, void*), void* memory, void* env, size_t element_byte_size): 
+    cmp(cmp), memory(memory), env(env), element_byte_size(element_byte_size) {}
+    bool (*cmp)(void*, void*, void*);
     int n=0;
+    size_t element_byte_size;
+
+    void sift_up(const int& u) {
+        int v = u;
+        while (v!=0){
+            if (cmp(memory + v * element_byte_size, memory + (v-1)/2 * element_byte_size, env)){
+                swap((memory + v * element_byte_size), (memory + (v-1)/2 * element_byte_size), element_byte_size);
+                v = (v-1)/2;
+            }
+            else break;
+        }
+    }
+
+    bool is_empty() {
+        return n==0;
+    }
+
+    void sift_down(const int& num){
+        int v = num;
+
+        while (2*v+1<=n-1){
+            int u = 2*v+1;
+            if (u+1 <= n - 1 && cmp(memory+(u+1)*element_byte_size, memory+u*element_byte_size, env)){
+                u++;
+            }
+            if (cmp(memory+u*element_byte_size, memory+v*element_byte_size, env)) {
+                swap(memory+u*element_byte_size, memory+v*element_byte_size, element_byte_size);
+                v = u;
+            }
+            else break;
+        }
+    }
+
+    void* get_min() {
+        return memory;
+    }
+
+    void insert(void* x){
+        memcpy(memory+n*element_byte_size, x, element_byte_size);
+        n++;
+        sift_up(n-1);
+        
+    }
+
+    void delete_min(){
+        if (is_empty()) cout<<"can't delete min";
+        else{
+            memcpy(memory, memory+element_byte_size*(n - 1), element_byte_size);
+            n--;
+            sift_down(0);
+        }
+    }
+
 };
 
-bool is_empty(Heap* heap){
-    return (*heap).n==0;
-}
 
-void swap(int *a, int *b) {
-    auto temp = *b;
-    *b = *a;
-    *a = temp;
-}
 
-void sift_up(Heap* heap, const int& u){
-    int v = u;
-    while (v!=0){
-        if ((*heap).start[v]<(*heap).start[(v-1)/2]){
-            swap(&(heap->start)[v], &(heap->start)[(v-1)/2]);
-            v = (v-1)/2;
-        }
-        else break;
+bool TestHeap1() {
+    int* start = new int[1000];
+    int* a = new int(4);
+    Heap h(&is_lower, start, nullptr, 32);
+    h.insert(a);
+    if (*(int*)h.get_min() != 4) {
+        delete[] start;
+        return false;
     }
-}
-
-void sift_down(Heap* heap, const int& num){
-    int v = num;
-    // for (int i = 0; i < heap->n; ++i) cout << heap->start[i] << ' '; cout << '\n';
-
-    while (2*v+1<=(*heap).n-1){
-        int u = 2*v+1;
-        if (u+1 <= (*heap).n - 1 && (*heap).start[u] > (*heap).start[u+1]){
-            u++;
-        }
-        if ((*heap).start[u]<(*heap).start[v]){
-            swap((*heap).start+u, (*heap).start+v);
-            v = u;
-        }
-        else break;
+    *a = 5;
+    h.insert(a);
+    if (*(int*)h.get_min() != 4) {
+        delete[] start;
+        return false;
     }
-}
-
-int get_min(Heap* heap){
-    return *(*heap).start;
-}
-
-void insert(Heap* heap, int x){
-    if (is_empty(heap)){*(*heap).start = x; (*heap).n++;}
-    else{
-        (*heap).n++;
-        (*heap).start[(*heap).n-1] = x;
-        sift_up(heap, (*heap).n-1);
+    *a = 3;
+    h.insert(a);
+    if (*(int*)h.get_min() != 3) {
+        delete[] start;
+        return false;
+    }    
+    *a = 2;
+    h.insert(a);
+    if (*(int*)h.get_min() != 2) {
+        delete[] start;
+        return false;
+    }    
+    h.delete_min();
+    if (*(int*)h.get_min() != 3) {
+        delete[] start;
+        return false;
     }
+    delete[] start;
+    return true;
 }
 
-void delete_min(Heap* heap){
-    if (is_empty(heap)) cout<<"can't delete min";
-    else{
-        (*heap).start[0] = (*heap).start[(*heap).n - 1];
-        (*heap).n--;
-        // cout << (*heap).start[0];
-        sift_down(heap, 0);
-        // cout << (*heap).start[0];
+bool is_lower_double(void* a, void* b, void* env) {
+    auto ia = *(double*)(a);
+    auto ib = *(double*)(b);
+    return ia < ib;
+}
+
+bool TestHeap2() {
+    double* start = new double[1000];
+    double* a = new double(1.2);
+    Heap h(&is_lower_double, start, nullptr, 8 * (sizeof *a));
+    h.insert(a);
+    if (*(double*)h.get_min() != 1.2) {
+        delete[] start;
+        return false;
     }
+    *a = 5.09;
+    h.insert(a);
+    if (*(double*)h.get_min() != 1.2) {
+        delete[] start;
+        return false;
+    }
+    *a = 0.76;
+    h.insert(a);
+    if (*(double*)h.get_min() != 0.76) {
+        delete[] start;
+        return false;
+    }    
+    *a = 2.0;
+    h.insert(a);
+    if (*(double*)h.get_min() != 0.76) {
+        delete[] start;
+        return false;
+    }    
+    h.delete_min();
+    if (*(double*)h.get_min() != 1.2) {
+        delete[] start;
+        return false;
+    }
+    delete[] start;
+    return true;
 }
 
-// void construct(Heap* h) {
-//     h->start = new int[1000];
-
-//     for (int i = 0; i < 1000; ++i) {
-//         h->start[i] = 0;
-//     }
-// }
 
 int main(){
-    int a = 3;
-    int* p = &a;
-    // cout << "1\n";
-    // Heap h;
-    // cout << "2\n";
-    // // construct(&h);
-    // insert(&h, 4);
-    // cout << get_min(&h) << endl;
-    // insert(&h, 2);
-    // cout << get_min(&h) << endl;
-    // insert(&h, 5);
-    // cout << get_min(&h) << endl;
-    // insert(&h, 1);
-    // cout << get_min(&h) << endl;
-    // // dbg
-    // delete_min(&h);
-    // // dbg
-    // cout << get_min(&h) << endl;
-    // return 0;
+    if (TestHeap2()) cout << "ok\n";
+    else cout<< "error\n";
 }
