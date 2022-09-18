ab6cc759135401aca2dd08776b3174cc323204a5
anna <anna8g5@mail.ru>
2022-09-18 16:35:38 +0300

    new functions

--- 09-15.cpp
+++ 09-15.cpp
@@ -51,18 +51,65 @@ struct Rectangle{
         return Rectangle(Point(point.minx(rha.point).x, point.miny(rha.point).y));
     }
 
+    Rectangle operator=(Rectangle const &rha){
+        return Rectangle(rha.point);
+    }
+
     void print () const{
         point.print();
     }
 };
 
-Rectangle* S(string str){
-    remove(str.begin(), str.end(), ' ');
-    cout << str << endl;
+Rectangle* R(string str){
+    string x, y;
+    int num_left, num_comma, num_right;
+    num_left = str.find('(');
+    num_comma = str.find(',');
+    num_right = str.find(')');
+    x = (str.substr(num_left, num_comma));
+    y = (str.substr(num_comma, num_left));
+    cout << x<<"_"<<y;
     Rectangle* rect;
     return rect;
 
+}
 
+Rectangle* M(string str){
+    Rectangle* rect;
+    int num_multi = str.find('*');
+
+    if (num_multi != string::npos){
+        string str1 = str.substr(0, num_multi);
+        string str2 = str.substr(num_multi+1);
+        cout << str1<< '_'<<str2 <<"_in"<< endl;
+        *rect = ((*R(str1)) * (*M(str2)));
+    }
+    else{
+        *rect = Rectangle((*R(str)).point);
+    }
+    return rect;
+}
+
+
+Rectangle* S(string str){
+    //delete waterspaces
+    int num_wat = count(str.begin(), str.end(), ' ');
+    int len = str.length();
+    remove(str.begin(), str.end(), ' ');
+    str.resize(len-num_wat);
+
+    Rectangle* rect;
+    int num_plus = str.find('+');
+    if (num_plus != string::npos){
+        string str1 = str.substr(0, num_plus);
+        string str2 = str.substr(num_plus+1);
+        cout << str1<< '_'<<str2 << endl;
+        *rect = Rectangle((*M(str1)) + (*S(str2)).point);
+    }
+    else{
+        *rect = Rectangle((*M(str)).point);
+    }
+    return rect;
 }
 
 
@@ -79,7 +126,8 @@ int main(){
     (rect3*rect2).print();
     cout<<endl;
     Rectangle* rect33;
-    rect33 = S("ff f");
+    string str="(3,5)+(6, 9)";
+    rect33 = S(str);
 
     return 0;
 }
