5609d02757999760fd35f00ae2adca235edf76fa
anna <anna8g5@mail.ru>
2022-10-25 22:46:16 +0300

    more changes

--- Conetest/C.cpp
+++ Conetest/C.cpp
@@ -17,19 +17,17 @@ private:
 
     friend bool operator==(const Fraction &lhs, const Fraction &rhs)
     {
-        return (((lhs.numerator == rhs.numerator) && (rhs.denominator == lhs.denominator)) 
-        ||((rhs.numerator == lhs.numerator) && (rhs.numerator==0)));
+        return (((lhs.numerator == rhs.numerator) && (rhs.denominator == lhs.denominator)) || ((rhs.numerator == lhs.numerator) && (rhs.numerator == 0)));
     }
 
     friend bool operator==(const Fraction &lhs, const int64_t &rhs)
     {
-        return (((lhs.numerator == rhs) && (1 == lhs.denominator)) ||((lhs.numerator == 0) && (rhs==0)));
+        return (((lhs.numerator == rhs) && (1 == lhs.denominator)) || ((lhs.numerator == 0) && (rhs == 0)));
     }
 
-
 public:
-    Fraction(int64_t numerator_out, uint64_t denominator_out) : numerator(numerator_out/gcd(numerator_out, denominator_out)),
-                                                             denominator(denominator_out/gcd(numerator_out, denominator_out)) {}
+    Fraction(int64_t numerator_out, uint64_t denominator_out) : numerator(numerator_out / gcd(abs(numerator_out), denominator_out)),
+                                                                denominator(denominator_out / gcd(abs(numerator_out), denominator_out)) {}
     Fraction() = delete;
     Fraction(const Fraction &rhs) : Fraction(rhs.numerator, rhs.denominator) {}
 
@@ -41,21 +39,12 @@ public:
         return *this;
     }
 
-    int64_t gcd(int64_t a, uint64_t b) { return (a == 0 ? b : gcd(b % a, a)); }
+    int64_t gcd(int64_t a, uint64_t b) const { return (a == 0 ? b : gcd(b % a, a)); }
 
-    int64_t num() const
+    Fraction operator+(const Fraction &rha) const
     {
-        return numerator;
-    }
-    uint64_t den() const
-    {
-        return denominator;
-    }
-
-    Fraction operator+(const Fraction &rha)
-    {
-        int64_t numer = rha.num() * denominator + numerator * rha.den();
-        uint64_t denom = rha.den() * denominator;
+        int64_t numer = rha.numerator * denominator + numerator * rha.denominator;
+        uint64_t denom = rha.denominator * denominator;
         int64_t gcd_n_d = gcd(abs(numer), denom);
         denom /= gcd_n_d;
         numer = numer / gcd_n_d;
@@ -63,16 +52,16 @@ public:
         return frac;
     }
 
-    Fraction& operator+=(const Fraction &rha)
+    Fraction &operator+=(const Fraction &rha) 
     {
-        *this = (*this)+rha;
+        *this = (*this) + rha;
         return *this;
     }
 
-    Fraction operator-(const Fraction &rha)
+    Fraction operator-(const Fraction &rha) const
     {
-        int64_t numer = rha.num() * denominator - numerator * rha.den();
-        uint64_t denom = rha.den() * denominator;
+        int64_t numer = rha.numerator * denominator - numerator * rha.denominator;
+        uint64_t denom = rha.denominator * denominator;
         int64_t gcd_n_d = gcd(abs(numer), denom);
         denom /= gcd_n_d;
         numer = numer / gcd_n_d;
@@ -80,10 +69,10 @@ public:
         return frac;
     }
 
-    Fraction operator*(const Fraction &rha)
+    Fraction operator*(const Fraction &rha) const
     {
-        int64_t numer = rha.num() * numerator;
-        uint64_t denom = rha.den() * denominator;
+        int64_t numer = rha.numerator * numerator;
+        uint64_t denom = rha.denominator * denominator;
         int64_t gcd_n_d = gcd(abs(numer), denom);
         denom /= gcd_n_d;
         numer = numer / gcd_n_d;
@@ -91,7 +80,7 @@ public:
         return frac;
     }
 
-    Fraction operator-()
+    Fraction operator-()const
     {
         int64_t numer = -1 * numerator;
         Fraction frac(numer, denominator);
@@ -104,10 +93,10 @@ public:
 
 int main()
 {
-    Fraction a(1,3);
+    Fraction a(1, 3);
     a += Fraction(2, 3);
     std::cout << (a == 1);
-    std::cout << ((Fraction(2, 7) + Fraction(12, 7)) == (int64_t)2)  << " ";
+    std::cout << ((Fraction(2, 7) + Fraction(12, 7)) == (int64_t)2) << " ";
 
     return 0;
 }
